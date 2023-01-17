5474e9d13bd0851f705bf723095d3661b54257e8
Rmarieta <raphael.marietan@epfl.ch>
2023-01-17 11:57:25 +0100

    Margin

--- frontend-ui/components/Feed.js
+++ frontend-ui/components/Feed.js
@@ -4,7 +4,7 @@ import Posts from "./Posts";
 
 const Feed = () => {
   return (
-    <div className="no-scrollbar flex-grow h-screen pt-6 mr-5 ml-2 overflow-y-auto bg-[#18191a]">
+    <div className="no-scrollbar flex-grow h-screen pt-6 mr-5 ml-2 mb-6 overflow-y-auto bg-[#18191a]">
       <div className="mx-auto max-w-md md:max-w-xl lg:max-w-2xl">
         {/* POST BOX */}
         <CreatePost />
